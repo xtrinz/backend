5ab44b8772c2836e5d54c900965eeffbe61e488f
ubaid2310 <ubaidrahman231@gmail.com>
2021-04-16 20:11:42 +0530

    cleanup

---
+++ database/shop/createshop.js
@@ -0,0 +1,67 @@
+const { tempShopInfoCollection, userCollection } = require("../connect");
+
+const createShop = async function (
+  user,
+  shopname,
+  location,
+  brandname,
+  shoptype,
+  certificates,
+  contactdetails
+) {
+  location = {
+    type: "Point",
+    coordinates: [location.lng, location.lat],
+    addressline: location.addressline,
+    feature: location.feature, // floor no. or complex name
+    landmark: location.landmark,
+  };
+  const insertOptions = {
+    shopname,
+    location,
+    brandname,
+    shoptype,
+    certificates,
+    contactdetails,
+    verificationstatus: "Not started",
+  };
+  const tempshopinfo = await tempShopInfoCollection.insertOne(insertOptions);
+  const query = {
+    _id: user._id,
+  };
+  const options = {
+    $push: {
+      tempshopinfoids: tempshopinfo.insertedId,
+    },
+  };
+  await userCollection.updateOne(query, options);
+};
+
+const verificationstatus = async function (user) {
+  const tempshopinfoids = user.tempshopinfoids;
+  let data = [];
+  for (const tempshopinfoid of tempshopinfoids) {
+    const query = {
+      tempshopinfoid,
+    };
+    const tempshopinfo = await tempShopInfoCollection.findOne(query);
+    const arrayData = {
+      shopname: tempshopinfo.shopname,
+      location: {
+        addressline: tempshopinfo.location.addressline,
+        feature: tempshopinfo.location.feature,
+        landmark: tempshopinfo.location.landmark,
+      },
+      shoptype: tempshopinfo.shoptype,
+      brandname: tempshopinfo.brandname,
+      verificationstatus: tempshopinfo.verificationstatus,
+    };
+    data.push(arrayData);
+  }
+  return data;
+};
+
+module.exports = {
+  createShop,
+  verificationstatus,
+};
---
+++ routes/customer/cart.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const code = require("../../error/code");
+const {
+  dataForCartPage,
+  addItemToCart,
+  deleteCartItem,
+} = require("../../database/customer/cart");
+const validator = require("../../validators/customer/cart");
+
+const router = express.Router();
+
+/**
+ * this api return cart data
+ */
+router.get("/", async (req, res, next) => {
+  try {
+    const { user } = req.body;
+    // getting data for cart page
+    const data = await dataForCartPage(user);
+    // send response
+    return res.status(code.OK).json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", validator.add_cart, async (req, res, next) => {
+  try {
+    const { user, shopinfoid, productsid, quantity } = req.body;
+    // call function to add item
+    await addItemToCart(user, shopinfoid, productsid, quantity);
+    // send success response
+    return res.status(code.OK).json({ message: "Item added to your cart" });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/", validator.delete_cart, async (req, res, next) => {
+  try {
+    const { user, cartitemid } = req.body;
+    await deleteCartItem(user, cartitemid);
+    return res.status(code.OK).json({ message: "item removed from your cart" });
+  } catch (error) {
+    next(error);
+  }
+});
+// missing quanity update route
+
+module.exports = router;
