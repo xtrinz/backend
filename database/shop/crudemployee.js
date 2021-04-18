365403a0b528b4f946212e750420a7ba3db52b2b
ubaid2310 <ubaidrahman231@gmail.com>
2021-04-19 02:59:01 +0530

    employee almost done

--- database/shop/crudemployee.js
+++ database/shop/crudemployee.js
@@ -60,7 +60,6 @@ const acceptedEmployee = async function (shopinfoid, user, invitepassword) {
   const options1 = {
     tempemployee: { $elemMatch: { userid: user._id } },
   };
-  let jobtitle;
   const shopinfo = await shopInfoCollection.findOne(query1, options1);
   const options2 = {
     $pull: {
@@ -79,11 +78,75 @@ const acceptedEmployee = async function (shopinfoid, user, invitepassword) {
     $pull: {
       notification: { shopinfoid, type: "shop employee invitation" },
     },
+    $push: {
+      shopinfoids: shopinfoid,
+    },
   };
   await userCollection.updateOne(query3, options3);
+};
+
+const rejectedEmployee = async function (shopinfoid, user) {
+  const query1 = {
+    _id: shopinfoid,
+    tempemployee: { userid: user._id },
+  };
+  const options1 = {
+    $pull: {
+      tempemployee: { userid: user._id },
+    },
+  };
+  await shopInfoCollection.updateOne(query1, options1);
+  const query2 = {
+    _id: user._id,
+    notification: { shopinfoid, type: "shop employee invitation" },
+  };
+  const options2 = {
+    $pull: {
+      notification: { shopinfoid, type: "shop employee invitation" },
+    },
+  };
+  await userCollection.updateOne(query2, options2);
+};
+
+const removeEmployee = async function (shopinfoid, userid) {
+  const query1 = {
+    _id: shopinfoid,
+    employee: { userid },
+  };
+  const options1 = {
+    $pull: {
+      employee: { userid },
+    },
+  };
+  await shopInfoCollection.updateOne(query1, options1);
+  const query2 = {
+    _id: userid,
+  };
+  const options2 = {
+    $pull: {
+      shopinfoids: shopinfoid,
+    },
+  };
+  await userCollection.updateOne(query2, options2);
+};
+
+const updateEmployee = async function (shopinfoid, userid, jobtitle) {
+  const query1 = {
+    _id: shopinfoid,
+    employee: { userid },
+  };
+  const options1 = {
+    $set: {
+      "employee.$.jobtitle": jobtitle,
+    },
+  };
+  await shopInfoCollection.updateOne(query1, options1);
 };
 
 module.exports = {
   addEmployee,
   acceptedEmployee,
+  rejectedEmployee,
+  removeEmployee,
+  updateEmployee,
 };
--- routes/shop/crudemployee.js
+++ routes/shop/crudemployee.js
@@ -1,22 +1,46 @@
 const {
   addEmployee,
   acceptedEmployee,
+  rejectedEmployee,
+  removeEmployee,
+  updateEmployee,
 } = require("../../database/shop/crudemployee");
+const code = require("../../error/code");
 
 const router = require("express").Router();
 
 // add parters and staff
 // Todo : invitation send
-router.post("/employee/add/", async (req, res, next) => {
+// required permission(owner)
+router.post("/add", async (req, res, next) => {
   const { shopinfoid, jobtitle, phonenumber } = req.body;
   await addEmployee(shopinfoid, jobtitle, phonenumber);
   return res.status(code.OK).json("Invitation send success fully");
 });
 
 //when employee accept invitation
-router.post("/employee/accept", async (req, res, next) => {
+router.post("/accept", async (req, res, next) => {
   const { shopinfoid, user, invitepassword } = req.body;
   await acceptedEmployee(shopinfoid, user, invitepassword);
+  return res.status(code.OK).json("success");
+});
+
+router.post("/reject", async (req, res, next) => {
+  const { shopinfoid, user } = req.body;
+  await rejectedEmployee(shopinfoid, user);
+  return res.status(code.OK).json("success");
+});
+// required permission(owner)
+router.delete("/remove", async (req, res, next) => {
+  const { shopinfoid, userid } = req.body;
+  await removeEmployee(shopinfoid, userid);
+  return res.status(code.OK).json("success");
+});
+// required permission(owner)
+// update jobtitle of a employee
+router.post("/update", async (req, res, next) => {
+  const { shopinfoid, userid, jobtitle } = req.body;
+  await updateEmployee(shopinfoid, userid, jobtitle);
   return res.status(code.OK).json("success");
 });
 
