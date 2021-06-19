1eff171e8289168caebbbb4be2f1128589741bc3
Yasir <dev.io.yasir@gmail.com>
2021-06-19 17:47:54 +0530

    modified madge

--- aid/cdeps.js
+++ aid/cdeps.js
@@ -1,12 +1,4 @@
-/*
- 	https://stackoverflow.com/questions/23875233/require-returns-an-empty-object/23875299
-	https://www.npmjs.com/package/madge
-
-	To print circular dependencies
-	------------------------------
-*/
-
-
+// Print circular dependencies
 const madge = require('madge')
 
 madge('./cmd/main.js').then((res) => 
